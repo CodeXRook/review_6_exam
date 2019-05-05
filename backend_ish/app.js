9e4d2de15d4e703041041ea591d8d20ef4f9e5ec
CodeXRook <lacekitfly@gmail.com>
2019-05-05 15:20:06 -0400

    updated apps

--- backend_ish/app.js
+++ backend_ish/app.js
@@ -1,2 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const {createUser, readAll, getSingleUser} = require('./routes/users');
+const {getAllShows, getShowsByGenreID, getAllShow, postNewshow} = require('./routes/shows');
+const {readAllGenres} = require('./routes/genres');
+const {readUserComment, postComment} = require('./routes/comments');
